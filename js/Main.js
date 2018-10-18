5a2c51d569c02f410af1edc38f466b3c38d0410e
Akigawa <sonoskeakigawa@gmail.com>
2018-10-18 13:36:56 -0700

    Readme

--- js/Main.js
+++ js/Main.js
@@ -1,43 +1,53 @@
-var cookiesnum = 0;
+var logs = 0;
 var cookieiterator = 1;
 var iteratoriterator = 1;
 var priceiterator = 10;
 var sawmills = 0;
 var sawmillstr = 1;
-
+var active = false;
+var counter = 1;
 function update()
 {
 
 }
 
 $(document).ready(function() {
-  $("form#some-form").submit(function(event) {
-    var someInput = $("input#some-input").val();
+active = true;
 
-    event.preventDefault();
-  });
-  document.getElementById("cookies2").innerHTML = cookiesnum;
+  logs += sawmills;
+  document.getElementById("cookies2").innerHTML = logs;
   document.getElementById("priceiterator").innerHTML = priceiterator;
-  document.getElementById("sawmills").innerHTML = priceiterator;
+  document.getElementById("sawmills").innerHTML = sawmills;
 
 });
 
 function cookies() {
-  document.getElementById("cookies2").innerHTML = cookiesnum;
+  document.getElementById("cookies2").innerHTML = logs;
 
-  cookiesnum += cookieiterator;
+  logs += cookieiterator;
 
 }
 function axe()
 {
 
-  if (cookiesnum>=priceiterator)
+  if (logs >= priceiterator)
   {
+logs -= priceiterator;
     priceiterator *= 2;
     cookieiterator += iteratoriterator;
-    cookiesnum -= 10;
 
+    document.getElementById("cookies2").innerHTML = logs;
       document.getElementById("priceiterator").innerHTML = priceiterator;
 
   }
+
+}
+
+function sawmill()
+{
+sawmills += 1;
+console.log (sawmills);
+document.getElementById("cookies2").innerHTML = logs;
+document.getElementById("priceiterator").innerHTML = priceiterator;
+      document.getElementById("sawmills").innerHTML = sawmills;
 }
